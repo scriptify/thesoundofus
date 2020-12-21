86fcd77bfae2dfb8272af5778a4d737d37771552
Maximilian Torggler <maximilian.torggler@tuta.io>
2020-12-21 12:54:13 +0100

    feat: added tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+
 import "../styles/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./pages/**/*.tsx", "./browser/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
