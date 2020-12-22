00e28295e48b5de1fedd08eabbb5e7d71d3907d3
Maximilian Torggler <maximilian.torggler@tuta.io>
2020-12-22 12:34:55 +0100

    feat: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Authentication from "../browser/components/Authentication";
 import { useEffect, useState } from "react";
 import ImagesGrid from "../browser/components/ImagesGrid";
 import Loading from "../browser/components/Loading";
+import Head from "next/head";
 
 function Home() {
   // function onPlayRandom() {
@@ -20,12 +21,24 @@ function Home() {
   }, []);
 
   // So... pretty useless to Next.js for this app
-  return didRender ? (
-    <main>
-      {rootStore.isAuthenticationNeeded ? <Authentication /> : <ImagesGrid />}
-    </main>
-  ) : (
-    <Loading />
+  return (
+    <>
+      <Head>
+        <title>The Sound of Us</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      {didRender ? (
+        <main>
+          {rootStore.isAuthenticationNeeded ? (
+            <Authentication />
+          ) : (
+            <ImagesGrid />
+          )}
+        </main>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
 
